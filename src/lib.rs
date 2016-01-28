f17cc2b3dd2ce7dfb6197f14965a02689935f33c
brian-js <brian.smith@maidsafe.net>
2016-01-28 17:20:49 +0000

    Generate random port's in tests with additional ten attempts on failure.

--- src/lib.rs
+++ src/lib.rs
@@ -432,16 +432,35 @@ mod tests {
     use std::thread;
     use std::sync::mpsc::{self, TryRecvError};
     use std::time::Duration;
+    use rand;
+    use rand::distributions::IndependentSample;
 
     #[test]
     fn three_peer_localhost_discovery() {
         let (tx0, _rx0) = mpsc::channel();
-        let sd0 = unwrap_result!(ServiceDiscovery::new(45666, 0u32));
+        let mut rng = rand::thread_rng();
+        let range = rand::distributions::Range::new(1024, 65535);
+        let mut port: u16 = range.ind_sample(&mut rng);
+        let mut sd0 = ServiceDiscovery::new(port, 0u32);
+
+        if sd0.is_err() {
+            for _ in 0..10 {
+                port = range.ind_sample(&mut rng);
+                if let Ok(sd) = ServiceDiscovery::new(port, 0u32) {
+                    sd0 = Ok(sd);
+                    break;
+                }
+            }
+        }
+
+        assert!(sd0.is_ok());
+        let sd0 = unwrap_result!(sd0);
+
         assert!(sd0.register_seek_peer_observer(tx0));
         assert!(sd0.set_listen_for_peers(true));
 
         let (tx1, rx1) = mpsc::channel();
-        let sd1 = unwrap_result!(ServiceDiscovery::new(45666, 1u32));
+        let sd1 = unwrap_result!(ServiceDiscovery::new(port, 1u32));
         assert!(sd1.register_seek_peer_observer(tx1));
         assert!(sd1.seek_peers());
 
@@ -455,13 +474,31 @@ mod tests {
     #[test]
     fn localhost_discovery_with_generator() {
         let (tx0, _rx0) = mpsc::channel();
-        let reply_gen = || 0u32;
-        let sd0 = unwrap_result!(ServiceDiscovery::new_with_generator(45667, reply_gen));
+        let reply_gen0 = || 0u32;
+        let mut rng = rand::thread_rng();
+        let range = rand::distributions::Range::new(1024, 65535);
+        let mut port: u16 = range.ind_sample(&mut rng);
+        let mut sd0 = ServiceDiscovery::new_with_generator(port, reply_gen0);
+
+        if sd0.is_err() {
+            for _ in 0..10 {
+                port = range.ind_sample(&mut rng);
+                if let Ok(sd) = ServiceDiscovery::new(port, 0u32) {
+                    sd0 = Ok(sd);
+                    break;
+                }
+            }
+        }
+
+        assert!(sd0.is_ok());
+        let sd0 = unwrap_result!(sd0);
+
         assert!(sd0.register_seek_peer_observer(tx0));
         assert!(sd0.set_listen_for_peers(true));
 
         let (tx1, rx1) = mpsc::channel();
-        let sd1 = unwrap_result!(ServiceDiscovery::new(45667, 1u32));
+        let reply_gen1 = || 1u32;
+        let sd1 = unwrap_result!(ServiceDiscovery::new_with_generator(port, reply_gen1));
         assert!(sd1.register_seek_peer_observer(tx1));
         assert!(sd1.seek_peers());
 
@@ -475,12 +512,29 @@ mod tests {
     #[test]
     fn localhost_discovery_stop_listening() {
         let (tx0, _rx0) = mpsc::channel();
-        let sd0 = unwrap_result!(ServiceDiscovery::new(45668, 0u32));
+        let mut rng = rand::thread_rng();
+        let range = rand::distributions::Range::new(1024, 65535);
+        let mut port: u16 = range.ind_sample(&mut rng);
+        let mut sd0 = ServiceDiscovery::new(port, 0u32);
+
+        if sd0.is_err() {
+            for _ in 0..10 {
+                port = range.ind_sample(&mut rng);
+                if let Ok(sd) = ServiceDiscovery::new(port, 0u32) {
+                    sd0 = Ok(sd);
+                    break;
+                }
+            }
+        }
+
+        assert!(sd0.is_ok());
+        let sd0 = unwrap_result!(sd0);
+
         assert!(sd0.register_seek_peer_observer(tx0));
         assert!(sd0.set_listen_for_peers(true));
 
         let (tx1, rx1) = mpsc::channel();
-        let sd1 = unwrap_result!(ServiceDiscovery::new(45668, 1u32));
+        let sd1 = unwrap_result!(ServiceDiscovery::new(port, 1u32));
         assert!(sd1.register_seek_peer_observer(tx1));
         assert!(sd0.set_listen_for_peers(false));
         assert!(sd1.seek_peers());
